cd1789f220b6d0f3668e0eab89673aacaa0dd032
Dmytro Chuchukalo <dimachuchukalo@gmail.com>
2020-08-07 08:34:51 +0300

    changed component Filter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
 import { connect } from 'react-redux';
-import actions from './redux/contacts/contacts-actions';
 import { ToastContainer, Zoom } from 'react-toastify';
 
 import styles from './App.module.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-const App = ({ items, resetFilter }) => {
-  items.length <= 1 && resetFilter();
-
+const App = ({ items }) => {
   return (
     <>
       <h1 className={styles.title}>Phonebook</h1>
@@ -39,13 +36,8 @@ const mapStateToProps = state => ({
   items: state.contacts.items,
 });
 
-const mapDispatchToProps = dispatch => ({
-  resetFilter: () => dispatch(actions.resetFilter()),
-});
-
 App.propTypes = {
   items: PropTypes.array.isRequired,
-  resetFilter: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, null)(App);
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
 import { connect } from 'react-redux';
 import actions from '../../redux/contacts/contacts-actions';
 
-const Filter = ({ filter, onChange }) => {
-  return (
-    <label className={styles.formTitle}>
-      Find contacts by name
-      <input
-        className={styles.formInput}
-        type="text"
-        value={filter}
-        onChange={onChange}
-      />
-    </label>
-  );
-};
+class Filter extends Component {
+  componentWillUnmount() {
+    this.props.resetFilter();
+  }
+  render() {
+    const { filter, onChange } = this.props;
+
+    return (
+      <label className={styles.formTitle}>
+        Find contacts by name
+        <input
+          className={styles.formInput}
+          type="text"
+          value={filter}
+          onChange={onChange}
+        />
+      </label>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   filter: state.contacts.filter,
@@ -24,11 +31,13 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   onChange: e => dispatch(actions.changeFilter(e.currentTarget.value)),
+  resetFilter: () => dispatch(actions.resetFilter()),
 });
 
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  resetFilter: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Filter);
